dfe8c9cd60150e1cdacd518700945b48f5ce1b64
SMarson03 <167792197+SMarson03@users.noreply.github.com>
2024-09-08 22:03:47 -0400

    part1

---
+++ SpringSoapDemo/src/main/java/com/persocholas/SpringSoapDemo/SpringSoapDemoApplication.java
@@ -0,0 +1,15 @@
+package com.persocholas.SpringSoapDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringSoapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSoapDemoApplication.class, args);
+	}
+
+}
